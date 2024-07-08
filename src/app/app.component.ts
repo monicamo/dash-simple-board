b1fe7fb057809edfbabdd2bbb2cfb8310fef8e41
Monica Monteiro <monica.monteiro@estadao.com>
2024-07-08 13:56:16 -0300

    Property Binding: Repetition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
@@ -8,6 +9,6 @@ import { TicketsComponent } from './dashboard/tickets/tickets.component';
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [ HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent ]
+  imports: [ DashboardItemComponent, HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent ]
 })
 export class AppComponent {}
