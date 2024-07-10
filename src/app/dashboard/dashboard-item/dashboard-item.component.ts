e8fbe627b40b4159302d0ebe45c39db72a4aeea0
Monica Monteiro <monica.monteiro@estadao.com>
2024-07-10 08:39:33 -0300

    Interacting with Host Elements via @HostListener & @HostBinding

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -6,9 +6,6 @@ import { Component, input } from '@angular/core';
   imports: [],
   templateUrl: './dashboard-item.component.html',
   styleUrl: './dashboard-item.component.css',
-  host: {
-    class: 'dashboard-item'
-  }
 })
 export class DashboardItemComponent {
   image = input.required<{ src: string, alt: string }>();
