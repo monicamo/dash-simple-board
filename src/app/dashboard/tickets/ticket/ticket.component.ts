6272dc8558954c95c72a8cb7e5cf2105e4038cd5
Monica Monteiro <monica.monteiro@estadao.com>
2024-07-10 14:20:17 -0300

    Revisiting Inputs & Signals

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Ticket } from './../tickets.model';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
 
 }
