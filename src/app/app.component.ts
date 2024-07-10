76aac79ffd5f816f455f2b0f36517a710691155c
Monica Monteiro <monica.monteiro@estadao.com>
2024-07-10 11:13:26 -0300

    Component Lifecycle - A Deep Dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,24 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
 import { TrafficComponent } from './dashboard/traffic/traffic.component';
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { LifecycleComponent } from './lifecycle/lifecycle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [ DashboardItemComponent, HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent ]
+  imports: [ LifecycleComponent, DashboardItemComponent, HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent ]
 })
-export class AppComponent {}
+export class AppComponent {
+
+  lifecycleComponentIsVisible = false;
+  lifecycleInputText = 'Some Random Number: ' + Math.random() * 100;
+
+  onToggleLifecycleComponentVisibility() {
+    this.lifecycleComponentIsVisible = !this.lifecycleComponentIsVisible;
+  }
+
+  onChangeLifecycleInputText() {
+    this.lifecycleInputText = 'Some Random Number: ' + Math.random() * 100;
+  }
+}
