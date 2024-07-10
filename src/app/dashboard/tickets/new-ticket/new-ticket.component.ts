b461dc79dc6c66904f9f07b35107792fbb511edc
Monica Monteiro <monica.monteiro@estadao.com>
2024-07-10 14:06:21 -0300

    Component Outputs: Repetition

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, output, Output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
+import { Ticket } from '../tickets.model';
 
 @Component({
   selector: 'app-new-ticket',
@@ -13,6 +14,9 @@ import { FormsModule } from '@angular/forms';
 export class NewTicketComponent implements AfterViewInit, OnInit {
   // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  // @Output() add = new EventEmitter<{ title: string, text: string }>();
+  add = output<{ title: string, text: string }>();
 
   ngOnInit(): void {
     console.log(' ngOnInit ')
@@ -26,5 +30,6 @@ export class NewTicketComponent implements AfterViewInit, OnInit {
 
   onSubmit(title: string, ticketText: string) {
     this.form().nativeElement.reset();
+    this.add.emit({ title: title, text: ticketText});
   }
 }
